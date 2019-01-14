2ba7edcd1972455abb6960e8aba02dd6ad05417a
troekens <troekens@gmail.com>
2019-01-14 20:54:38 +0100

    Back-end done

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,7 +1,11 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+// ••• Notice that "W" is uppercase ==> Good practice to put uppercase when we require a constructor
+const web3 = new Web3(ganache.provider());// Here we use lowercase for instance
+// ••• Here we create a new instance of W3 for our project.
+// ••• Provider is used to connect to a specific Ethereum Network - Mandatory
 
 const { interface, bytecode } = require('../compile');
 
@@ -9,16 +13,19 @@ let lottery;
 let accounts;
 
 beforeEach(async () => {
+    // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-
+    // Use one of those accounts to deploy
+    // the contract
     lottery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Lottery Contract', () => {
+
     it('deploys a contract', () => {
-        assert.ok(lottery.options.address);
+        assert.ok(lottery.options.address); //Check that the address property is ok/exist
     });
 
     it('allows one account to enter', async () => {
@@ -65,16 +72,16 @@ describe('Lottery Contract', () => {
                 from: accounts[0],
                 value: 0
             });
-            assert(false);
+            assert(false); //If there is no error. We still need to assert something.
         } catch (err) {
-            assert(err);
+            assert(err); //We assert the generated error. Check is already done within enter() function : require(msg.value > .01 ether);
         }
     });
 
     it('only manager can call pickWinner', async () => {
         try {
             await lottery.methods.pickWinner().send({
-                from: accounts[1]
+                from: accounts[1] //Manager is account[0] here
             });
             assert(false);
         } catch (err) {
